2d75e02701ac2d48f79abf7b5623f5e29e92492e
Thomas Li <ThomasLiProgramming@gmail.com>
2024-01-09 14:16:42 +0800

    basic

---
+++ Map.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Map{
+    private roadSegment[] roadMap = new roadSegment[100];
+
+    for(int i = 0; i<100; i++){
+        roadMap[i]
+    }
+}
---
+++ roadSegment.java
@@ -0,0 +1,18 @@
+public class roadSegment {
+    private int trafficLevel;
+    private int exitTime;
+
+    public roadSegment(int trafficLevelR){
+        this.trafficLevel = trafficLevelR;
+        this.exitTime = exitTime;
+    }
+
+    public int trafficLevel(){
+        return trafficLevel;
+    }
+
+    public int exitTime(){
+        return exitTime;
+    }
+
+}
